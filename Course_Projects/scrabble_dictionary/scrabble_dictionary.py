200632e70b595fa6e0ead91e2e93eddfa25f352d
Matthew Duffy <matthewfduffy@gmail.com>
2021-07-21 04:39:01 -0500

    adding scrabble dictionary project

--- Course_Projects/scrabble_dictionary/scrabble_dictionary.py
+++ Course_Projects/scrabble_dictionary/scrabble_dictionary.py
@@ -1,47 +1,47 @@
-"""
-    # Scrabble Dictionary
-
-    In this project, you will process some data from a group of friends playing scrabble. 
-    You will use dictionaries to organize players, words, and points.
-
-    Two lists are provided, letters and points. 
-    Combine these two into a dictionary that would map a letter to its point value.
-"""
-letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", 
-            "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", 
-            "U", "V", "W", "X", "Y", "Z"]
-
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
 
-# using zip and list comprehension to create a mapping between letters and points
 letters_to_points = dict(zip(letters, points))
 
-# adding an option for blank tiles
-letters_to_points[' '] = 0
+letters_to_points[" "] = 0
+# print(letters_to_points)
 
-# create a function that will take in a word and return how many points that word is worth
 def score_word(word):
     point_total = 0
     for letter in word:
-        point_total += letters_to_points.get(letter.upper())
+      point_total += letters_to_points.get(letter.upper(), 0)
+    #   print(f"The current letter is: {letter}. The current total is: {point_total}.")
     return point_total
 
-# Scoring a Game - Creating a dictionary that maps players to a list of words they have played
+
+# brownie_points = score_word("BROWNIE")
+# print(brownie_points)
+
 player_to_words = {
-                    "player1": ["BLUE", "TENNIS", "EXIT"],
-                    "wordNerd": ["EARTH", "EYES", "MACHINE"],
-                    "Lexi Con": ["ERASER", "BELLY", "HUSKY"],
-                    "Prof Reader": ["ZAP", "COMA", "PERIOD"]
-}
+  "player1": ["BLUE", "TENNIS", "EXIT"],
+  "wordNerd": ["EARTH", "EYES", "MACHINE"],
+  "Lexi Con": ["ERASER", "BELLY", "HUSKY"],
+  "Prof Reader": ["ZAP", "COMA", "PERIOD"]
+  }
 
 player_to_points = {}
-
-for player in player_to_words:
+  
+for player, words in player_to_words.items():
     player_points = 0
-    for word in player_to_words:
-        player_points += score_word(word)
-        print(player_points)
+    for word in words:
+        x = score_word(word)
+        player_points += x
+        print(f"{player}, {player_points}")
+    player_to_points[player] = player_points
+    print("------")
+    for each_key, each_value in player_to_points.items():
+        print(f"{each_key}, {each_value}")
 
-# For Testing
-# print(letters_to_points)
-# print(score_word("brownie"))
+
+
+# If you want extended practice, try to implement some of these ideas with the Python you’ve learned:
+
+#     play_word() — a function that would take in a player and a word, and add that word to the list of words they’ve played
+#     update_point_totals() — turn your nested loops into a function that you can call any time a word is played
+#     make your letter_to_points dictionary able to handle lowercase inputs as well
+
